eb0b42183c03d62a66a1dd1af3053557f48b274f
LinjunJiang23 <mayljjiang@gmail.com>
2025-04-03 11:50:47 -0400

    Updated

--- main/src/database/dev.js
+++ main/src/database/dev.js
@@ -1,16 +1,17 @@
 const { exec, execSync } = require('child_process');
 require('dotenv').config(); 
 
+
 function isProxyRunningLinux() {
     try {
-		const output = execSync('pgrep "cloud-sql-proxy"').toString().trim();
+		const output = execSync('pgrep -f "cloud-sql-proxy" | grep -v $$ || true').toString().trim();
 		console.log("This is the output of whether proxy is running: \n", output);
 		return output.length > 0;
     } catch (error) {
+		console.log("Encounter error: ", error);
         return false; // No process found
     }
-}
-
+};
 
 function isProxyRunningWindows() {
 	try {
@@ -21,21 +22,40 @@ function isProxyRunningWindows() {
     } catch (error) {
         return false; // No process found
     }
-}
+};
 
+function getProxyPID() {
+	try {
+		const output = execSync('pgrep "cloud-sql-proxy"').toString().trim();
+		return output;
+	} catch (error) {
+		console.log("Unknown error during pid fetching: ", error);
+		return;
+	}
+};
+
+function stopProxy() {
+	try {
+		let pid = getProxyPID();
+		if (typeof pid !== 'number') pid = parseInt(pid);
+		const output = execSync(`kill -9 ${pid}`);
+	} catch (error) {
+		console.error("Kill failed: ", error);
+	}
+};
 
 function startServer() {
 	const isLinux = (process.platform === "linux");
-	const credsPath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
-	const proxyPath = isLinux ? '../server/cloud-sql-proxy' : '../server/cloud-sql-proxy.exe';
-	const connectionName = process.env.CONNECTION_NAME;
+	const credsPath = "./server/key.json";
+	const proxyPath = isLinux ? './server/cloud-sql-proxy' : './server/cloud-sql-proxy.exe';
+	const connectionName = 'rich-chimera-455016-u7:us-central1:root';
 	const result = isLinux ? isProxyRunningLinux() : isProxyRunningWindows();
 	if (result) {
 		console.log("Cloud SQL Proxy is already running. Skipping startup.");
         return;
 	} else {
 		const command = isLinux ? 
-			`nohup ${proxyPath} --credentials-file="${credsPath}" "${connectionName}" > database/proxy.log 2>&1 &` :
+			`nohup ${proxyPath} --credentials-file="${credsPath}" "${connectionName}" > proxy.log 2>&1 &` :
 			`start "" "${proxyPath}" --credentials-file="${credsPath}" ${connectionName}`;
 		exec(command, (error, stdout, stderr) => {
 			if (error) {
@@ -51,4 +71,4 @@ function startServer() {
 	}
 };
 
-module.exports = { startServer };
+module.exports = { startServer, stopProxy };
--- main/src/database/start.js
+++ main/src/database/start.js
@@ -1,19 +1,21 @@
+const { startServer } = require('./dev.js');
+require('dotenv').config();
 const mysql = require('mysql2');
+
+startServer();
 
 const db = mysql.createConnection({
 	  host: "127.0.0.1",
 	  user: "root",
 	  password: "root6666",
 	  port: 3306,
-	});
-
-db.connect( (error) => {
-	  if (error) {
-		console.log(error);
-	  }
-	  else {
-		console.log("MySQL is connected!!!");
-	  }
 });
 
-module.exports = db;
+db.connect((error) => {
+	if (error) {
+		console.log(error);
+	}
+	else {
+		console.log("MySQL is connected!!!");
+	}
+});
