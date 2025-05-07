56678cc5e5ced7f92c29989aa75a3b6be43c47be
May Jiang <111789855+MayJiang23@users.noreply.github.com>
2025-05-06 23:12:04 -0400

    Update apiConfig.js

--- main/src/public/services/config/apiConfig.js
+++ main/src/public/services/config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from 'https://cdn.skypack.dev/axios';
 const api = axios.create({
-  baseURL: 'http://localhost:6003',
+  baseURL: 'https://ffxiv-api-app-150199820340.us-central1.run.app',
   headers: {
 	'Content-Type': 'application/json',
   },
